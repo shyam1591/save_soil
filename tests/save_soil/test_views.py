0a6b067e2d6be9a979510f0b1997d3bdd6d2f9fe
Shyam Mistry <sam_for_u@live.com>
2022-06-05 15:43:13 -0400

    fixing the code style

--- tests/save_soil/test_views.py
+++ tests/save_soil/test_views.py
@@ -1,6 +1,6 @@
+"""Test case that reponse to incorrect https request"""
 def test_index_ok(client):
-    # Make a GET request to / and store the response object
-    # using the Django test client.
+    """Making GET request to / and stored the response
+    assert the status to 200 if the resquest is ok"""
     response = client.get('/')
-    # Assert that the status_code is 200 (OK)
     assert response.status_code == 200
